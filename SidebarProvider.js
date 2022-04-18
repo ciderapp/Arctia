110d88f3e1485cff8f7b88921323957481e7ab86
Amaru8 <52407090+Amaru8@users.noreply.github.com>
2022-04-18 19:31:52 +0200

    Hide Reload button

--- SidebarProvider.js
+++ SidebarProvider.js
@@ -131,9 +131,8 @@ class SidebarProvider {
             value: ''
           });
       ">Previous Song</button>
-      <button id="reload-button" onclick="
-        /*
-        TODO: Try both.
+      <button id="reload-button" style="display: none;" onclick="
+        /*No need for this.
         document.location.reload()
         document.location = document.location
         */
