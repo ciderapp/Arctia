3c7e6337c66bd4b18966d9aed6e5d88b383c0b34
Amaru8 <52407090+Amaru8@users.noreply.github.com>
2022-04-19 17:31:56 +0200

    Remove some things in SidebarProvider

--- SidebarProvider.ts
+++ SidebarProvider.ts
@@ -35,16 +35,6 @@ export class SidebarProvider implements vscode.WebviewViewProvider {
           previous();
           break;
         }
-        case "onInfo": {
-          if (!mData.value) { return; }
-          vscode.window.showInformationMessage(mData.value);
-          break;
-        }
-        case "onError": {
-          if (!mData.value) { return; }
-          vscode.window.showErrorMessage(mData.value);
-          break;
-        }
       }
     });
   }
@@ -69,10 +59,6 @@ export class SidebarProvider implements vscode.WebviewViewProvider {
 			<html lang="en">
 			<head>
 				<meta charset="UTF-8">
-				<!--
-					Use a content security policy to only allow loading images from https or from our extension directory,
-					and only allow scripts that have a specific nonce.
-        -->
         <meta http-equiv="Content-Security-Policy" content="img-src https: data:; 'unsafe-inline'>
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
 				<link href="${stylesResetUri}" rel="stylesheet">
